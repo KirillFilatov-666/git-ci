601d3e4ff60b357e2b7f459754e3cd85b5283d44
Kirill69 <k.filatov@starfish24.com>
2025-09-20 21:23:21 +0300

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,6 +10,7 @@ load_dotenv()
 def driver(request):
     options = webdriver.ChromeOptions()
     options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
     options.add_argument("--window-size=1920x1080")
     options.add_argument("--disable-gpu")
     options.add_argument("--disable-dev-shm-usage")
